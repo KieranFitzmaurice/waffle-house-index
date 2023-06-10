c271a13536c07baf292bac112513642353dd9740
KieranFitzmaurice <kpf10@pitt.edu>
2023-06-10 03:22:32 +0000

    added proxies to main script

--- scrape_data.py
+++ scrape_data.py
@@ -13,11 +13,11 @@ proxy_list_path = os.path.join(pwd,'proxies','proxy_list.txt')
 proxypool = ws.ProxyPool(proxy_list_path)
 
 # Scrape and clean data on bojangles locations
-raw_filepath,scraper_issues = ws.get_bojangles_data()
+raw_filepath,scraper_issues = ws.get_bojangles_data(proxypool)
 df1 = ws.clean_bojangles_data(raw_filepath,scraper_issues)
 
 # Scrape and clean data on dunkin locations
-raw_filepath,scraper_issues = ws.get_dunkin_data()
+raw_filepath,scraper_issues = ws.get_dunkin_data(proxypool)
 df2 = ws.clean_dunkin_data(raw_filepath,scraper_issues)
 
 # Read in low-resolution grid for wendy's data
@@ -25,5 +25,5 @@ grid_filepath = os.path.join(pwd,'grids/usa_grid_v1.csv')
 grid = pd.read_csv(grid_filepath,index_col=0)
 
 # Scrape and clean data on wendy's locations
-raw_filepath,scraper_issues = ws.get_wendys_data(grid)
+raw_filepath,scraper_issues = ws.get_wendys_data(grid,proxypool)
 df3 = ws.clean_wendys_data(raw_filepath,scraper_issues)
