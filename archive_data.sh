6c87350d83c48fecf8820ec08c80d4bd1c349f0b
KieranFitzmaurice <kpf10@pitt.edu>
2023-06-11 13:41:18 -0400

    modified archival script

--- archive_data.sh
+++ archive_data.sh
@@ -3,6 +3,7 @@
 cd /home/kieran/projects/waffle-house-index
 source waffle-venv-1/bin/activate
 ARCHIVE="$(date '+%Y-%m-%d')_archived_data.tar.gz"
+LASTARCHIVE="$(date -d "-1 month" +%Y-%m-%d)_archived_data.tar.gz"
 tar -czvf $ARCHIVE data
 FILEPATH=$PWD/$ARCHIVE
 cd google_drive
@@ -12,6 +13,7 @@ cd ..
 rm -r data
 if [ $STATUS == 0 ]; then
   echo "Data successfully backed up to google drive"
+  rm $LASTARCHIVE
 else
   echo "Backup failed"
 fi
